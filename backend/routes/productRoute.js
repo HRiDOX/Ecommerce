96550aac892de457158cf62a526551200d30f55f
HRiDOX <rgrrf123@gmail.com>
2022-06-11 12:44:23 +0600

    Ecommerce

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
+const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
+
+const router = express.Router();
+
+//Product getting route
+router.route("/products").get( getAllProducts);
+//Product updating route
+router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
+//Product Updating , deleting and a single Product details
+router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
+router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
+module.exports = router
